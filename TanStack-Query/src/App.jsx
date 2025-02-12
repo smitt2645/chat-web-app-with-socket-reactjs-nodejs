fd5883a216675965753f7a320192f40a67190267
Smit Trivedi <smit.iihglobal@gmail.com>
2025-02-12 09:33:04 +0530

    first commit !

--- TanStack-Query/src/App.jsx
+++ TanStack-Query/src/App.jsx
@@ -4,7 +4,24 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Layout from "./Layout/Layout";
+import {io} from "socket.io-client"
+import { useEffect } from "react";
 function App() {
+  const socket = io('http://localhost:8000')
+  useEffect(()=>{
+    socket.on('connect',()=>{
+      console.log(`you are connected with my web application`)
+    })
+    socket.on('event1',(e)=>{
+      console.log("new message :",e)
+    })
+    socket.on('event2',(e)=>{
+      console.log("new message from:",e)
+    });
+    return () =>{
+      socket.disconnect();
+    };
+  },[])
   const router = createBrowserRouter([
     {
       path: "/",
---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+import {createServer} from "http"
+import cors from "cors"
+const app = express();
+const socket = new createServer(app);
+dotenv.config({
+    path:"./.env"
+})
+const io = new Server(socket,{
+    cors:{
+        origin:"http://localhost:5173",
+        methods:['GET','POST','PUT'],
+        credentials:true 
+    }
+})
+// this is a sercuit here we can get socket 
+io.on('connection',(socket)=>{
+    console.log('user connecting')
+    console.log("Id",socket.id)
+    // it will fire to all
+    // socket.emit('event1',`welcome ${socket.id}`)
+
+    // it will fire to all with sender's id but not to sender who has fired !
+    // socket.broadcast.emit('event2',`you recieved messege from ${socket.id} `)
+
+    // dissconnect socket !
+    socket.on('disconnect',()=>{
+        console.log('dis connected user : ',socket.id)
+    })
+
+})
+const port = process.env.PORT || 3000
+app.use(cors({
+    origin:"http://localhost:5173",
+}))
+socket.listen(port,()=>{
+    console.log(`your server is listening on http://localhost:${port}`)
+});
+
+app.get('/',(req,res)=>{
+        res.status(200).send('welcome to socket Io')
+})
